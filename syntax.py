dd594a8b67d12367b3a65387793d9ca32f49bbe6
asudegrkn <asudegr@gmail.com>
2022-01-31 17:48:18 +0300

    add python syntax

---
+++ syntax.py
@@ -0,0 +1,33 @@
+# python temel sözdizimi (syntax)
+
+# Büyük ve küçük harf
+ temel = 6
+ Temel = 7
+
+#yorum 
+ """
+ yorum 
+ 
+ """
+ 
+ #girintiler (conditions ,loops...)
+ 
+ if 5 < 10:
+     print("yes")
+ 
+ else:
+     print("no")
+ 
+ #anahtar kelimeler
+de= 45
+# def = 56 hatalı 
+
+
+#sayili değişken
+
+sayi1= 65
+sayi2 = 434
+
+# 1sayi=444 hatalı
+
+
