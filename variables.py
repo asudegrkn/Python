ca9333690d7d62111d8a2e31c366cd7934749ae3
asudegrkn <asudegr@gmail.com>
2022-01-31 17:39:39 +0300

    add variables with python

---
+++ variables.py
@@ -0,0 +1,27 @@
+
+# %%  değişkenler
+
+tamsayi_degiskeni = 10
+ondalik_sayi = 125.55
+
+print(tamsayi_degiskeni)
+
+
+# 4 işlem özellikleri
+
+pi_sayisi = 3.14
+katsayi = 2
+
+toplam = pi_sayisi + 1
+
+fark = pi_sayisi - 1
+
+carpma = pi_sayisi*katsayi
+
+bolme = pi_sayisi / katsayi
+
+print("toplam:", toplam)
+
+print ("toplam: {} ve fark: {}" .format(toplam,fark))
+
+print("çarpma: %.1f , bölme: %.4f" %(carpma , bolme))
