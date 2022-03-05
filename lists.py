33674acf3a235a78568adcb32e842f43228ff77d
asudegrkn <61904935+asudegrkn@users.noreply.github.com>
2022-03-05 21:24:46 +0300

    add lists with python

--- lists.py
+++ lists.py
@@ -16,5 +16,28 @@ print(hafta[0])
 #son eleman 
 
 print(hafta[6])
-print(len(hafta))
+print(len(hafta))  #eleman sayısı
 print(hafta[len(hafta)-1])
+print(hafta[-1])
+
+#liste 2-34 ; 1,2,3 indeks
+print(hafta[1:4]) #1den 4 e kadar 1 dahil 4 değil
+
+#sayı listesi
+sayi_listesi = [1,2,3,4,5,6]
+sayi_listesi.append(7)  #eleman ekleme
+
+print(sayi_listesi)
+
+#eleman çıkartma
+sayi_listesi.remove(4)
+print(sayi_listesi)
+
+#listeyi ters çevir
+sayi_listesi.reverse()
+print(sayi_listesi)
+
+#listeyi sırala
+sayi_listesi = [11,1,2,3,4,60,7,0]
+sayi_listesi.sort()
+print(sayi_listesi)
