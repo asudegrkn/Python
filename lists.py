0187fd1f2ba6e4459db1db38a570fe61c3001956
asudegrkn <asudegr@gmail.com>
2022-03-05 21:22:46 +0300

    add lists with python

---
+++ lists.py
@@ -0,0 +1,20 @@
+# Listeler
+
+"""
+composite data type ve çok yönlüdür.
+farklı data typeları içerisinde barındırabilir.
+
+"""
+
+liste = [1,2,3,4,5,6]
+print(type(liste))
+
+hafta = ["pazartesi","salı","çarşamba","perşembe","cuma","cumartrsi","pazar"]
+#ilk eleman
+print(hafta[0])
+
+#son eleman 
+
+print(hafta[6])
+print(len(hafta))
+print(hafta[len(hafta)-1])
