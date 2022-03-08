59e9cae063ecde922658e982aded350104220643
asudegrkn <asudegr@gmail.com>
2022-03-08 17:32:58 +0300

    add dictionary with python

---
+++ dictionary.py
@@ -0,0 +1,30 @@
+# dictionary (sözlük)
+
+"""
+
+-bir çeşit karma tablo türüdür
+-anahtar ve değer çiftlerinden oluşur
+- {"anahtar: değer"}
+
+
+"""
+
+dictionary = {"İstanbul": 34,
+              "İzmir"   : 35,
+              "Konya"   : 42}
+
+print(dictionary)
+
+#istanbul anahtarının (key) değeri
+#büyük küçük harfe duyarlı
+
+print(dictionary["İstanbul"])
+
+#anahtarlar
+print(dictionary.keys())
+
+#değerler
+print(dictionary.values())
+
+
+
